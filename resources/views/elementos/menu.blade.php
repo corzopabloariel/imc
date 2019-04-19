e5fa9eb060332ff2d5839d6903ff0717d75aca1f
Pablo Corzo <corzo.pabloariel@gmail.com>
2019-04-18 22:11:27 -0300

    2211

--- resources/views/elementos/menu.blade.php
+++ resources/views/elementos/menu.blade.php
@@ -1,97 +1,165 @@
 <div class="sidebar-header">
     <h3 class="m-0">Menu</h3>
 </div>
-<div class="position-relative" style="height: calc(100% - 73px - 38px); overflow-y:auto;">
+<div class="position-relative" style="height: calc(100% - 73px); overflow-y:auto;">
     <div class="w-100 position-absolute">
         <ul class="list-unstyled components m-0 p-0">
             <li class="">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-home"></i>Home</a>
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-home"></i>
+                    <span class="nav-label">Home</span>
+                </a>
                 <ul class="collapse list-unstyled" id="homeSubmenu">
                     <li>
-                        <a  href="{{ route('contenido.edit', ['seccion' => 'home'])}}">Contenido</a>
+                        <a href="{{ route('contenido.edit', ['seccion' => 'home'])}}">
+                            <i class="fas fa-file-contract"></i>
+                            <span class="nav-label">Contenido</span>
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('slider.index', ['seccion' => 'home'])}}">Slider</a>
+                        <a href="{{ route('slider.index', ['seccion' => 'home'])}}">
+                            <i class="fas fa-spider"></i>
+                            <span class="nav-label">Slider</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-            <a href="#nosotrosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-building"></i>Nosotros</a>
+                <a href="#nosotrosSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-building"></i>
+                    <span class="nav-label">Nosotros</span>
+                </a>
                 <ul class="collapse list-unstyled" id="nosotrosSubmenu">
                     <li>
-                        <a href="{{ route('contenido.edit', ['seccion' => 'nosotros'])}}">Contenido</a>
+                        <a href="{{ route('contenido.edit', ['seccion' => 'nosotros'])}}">
+                            <i class="fas fa-file-contract"></i>
+                            <span class="nav-label">Contenido</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-            <a href="#calidadSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-feather-alt"></i>calidad</a>
+                <a href="#calidadSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-feather-alt"></i>
+                    <span class="nav-label">calidad</span>
+                </a>
                 <ul class="collapse list-unstyled" id="calidadSubmenu">
                     <li>
-                        <a href="{{ route('contenido.edit', ['seccion' => 'calidad'])}}">Contenido</a>
+                        <a href="{{ route('contenido.edit', ['seccion' => 'calidad'])}}">
+                            <i class="fas fa-file-contract"></i>
+                            <span class="nav-label">Contenido</span>
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('archivo.index', ['seccion' => 'calidad'])}}">Archivos</a>
+                        <a href="{{ route('archivo.index', ['seccion' => 'calidad'])}}">
+                            <i class="fas fa-file-invoice"></i>
+                            <span class="nav-label">Archivos</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#serviciosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-project-diagram"></i>Servicios</a>
+                <a href="#serviciosSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-project-diagram"></i>
+                    <span class="nav-label">Servicios</span>
+                </a>
                 <ul class="collapse list-unstyled" id="serviciosSubmenu">
                     <li>
-                        <a href="{{ route('servicios.index')}}">Todos los Servicios</a>
+                        <a href="{{ route('servicios.index')}}">
+                            <i class="fab fa-servicestack"></i>
+                            <span class="nav-label">Todos los Servicios</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#prensaSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-newspaper"></i>Prensa</a>
+                <a href="#prensaSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-newspaper"></i>
+                    <span class="nav-label">Prensa</span>
+                </a>
                 <ul class="collapse list-unstyled" id="prensaSubmenu">
                     <li>
-                        <a href="{{ route('contenido.edit', ['seccion' => 'prensa'])}}">Contenido</a>
+                        <a href="{{ route('contenido.edit', ['seccion' => 'prensa'])}}">
+                            <i class="fas fa-file-contract"></i>
+                            <span class="nav-label">Contenido</span>
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('archivo.index', ['seccion' => 'prensa'])}}">Archivos</a>
+                        <a href="{{ route('archivo.index', ['seccion' => 'prensa'])}}">
+                            <i class="fas fa-file-invoice"></i>
+                            <span class="nav-label">Archivos</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#trabajosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-camera-retro"></i>Portfolio</a>
+                <a href="#trabajosSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-camera-retro"></i>
+                    <span class="nav-label">Portfolio</span>
+                </a>
                 <ul class="collapse list-unstyled" id="trabajosSubmenu">
                     <li>
-                        <a href="{{ route('familia.index')}}">Familia de Trabajos</a>
+                        <a href="{{ route('familia.index')}}">
+                            <i class="fas fa-briefcase"></i>
+                            <span class="nav-label">Familia de Trabajos</span>
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('familia.trabajo.index')}}">Todos los Trabajos</a>
+                        <a href="{{ route('familia.trabajo.index')}}">
+                            <i class="fas fa-network-wired"></i>
+                            <span class="nav-label">Todos los Trabajos</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#rrhhSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-id-card"></i>RR.HH.</a>
+                <a href="#rrhhSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-id-card"></i>
+                    <span class="nav-label">RR.HH.</span>
+                </a>
                 <ul class="collapse list-unstyled" id="rrhhSubmenu">
                     <li>
-                        <a href="{{ route('rrhh.index')}}">Listado de trabajos</a>
+                        <a href="{{ route('rrhh.index')}}">
+                            <i class="fas fa-list-ol"></i>
+                            <span class="nav-label">Listado de trabajos</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#clientesSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-users"></i>Clientes</a>
+                <a href="#clientesSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-users"></i>
+                    <span class="nav-label">Clientes</span>
+                </a>
                 <ul class="collapse list-unstyled" id="clientesSubmenu">
                     <li>
-                        <a href="{{ route('cliente.index')}}">Todos los clientes</a>
+                        <a href="{{ route('cliente.index')}}">
+                            <i class="fas fa-list-ol"></i>
+                            <span class="nav-label">Todos los clientes</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li><hr/></li>
             <li>
-                <a href="#clientesUsuariosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-user-shield"></i>Clientes</a>
+                <a href="#clientesUsuariosSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-user-shield"></i>
+                    <span class="nav-label">Clientes</span>
+                </a>
                 <ul class="collapse list-unstyled" id="clientesUsuariosSubmenu">
                     <li>
-                        <a href="{{ route('cliente.clientes')}}">Listado de clientes</a>
+                        <a href="{{ route('cliente.clientes')}}">
+                            <i class="fas fa-list-ol"></i>
+                            <span class="nav-label">Listado de clientes</span>
+                        </a>
                     </li>
                 </ul>
             </li>
             <li><hr/></li>
             <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">IMC</a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <span class="nav-label">IMC</span>
+                </a>
                 <ul class="collapse list-unstyled" id="pageSubmenu">
                     <li>
                         <a href="{{ route('empresa.index')}}">Datos</a>
@@ -110,14 +178,19 @@
                     @endif
                 </ul>
             </li>
+            <li><hr/></li>
+            <li>
+                <a class="bg-danger text-white" data-toggle="collapse" aria-expanded="false" href="{{route('adm.logout')}}">
+                    <i class="fas fa-power-off text-white"></i>
+                    <span class="nav-label">Salir</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://osole.freshdesk.com/support/home" data-toggle="collapse" aria-expanded="false" target="_blank">
+                <i class="fas fa-ticket-alt"></i>
+                    <span class="nav-label">Soporte</span>
+                </a>
+            </li>
         </ul>
-    </div>
-</div>
-<div class="row m-0 position-absolute w-100" style="bottom: 0; left: 0;">
-    <div class="col-12 col-md-6 px-0">
-        <a href="https://osole.freshdesk.com/support/home" target="_blank" class="btn-gds py-2 btn-block text-uppercase text-center"><i class="fas fa-ticket-alt mr-2"></i>soporte</a>
-    </div>
-    <div class="col-12 col-md-6 px-0">
-        <a href="{{route('adm.logout')}}" class="btn-danger btn-block py-2 text-uppercase text-center"><i class="fas fa-power-off mr-2"></i>Salir</a>
     </div>
 </div>
